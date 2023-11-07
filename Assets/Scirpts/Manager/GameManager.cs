54b427e7f475c2e7b0438a23d88b1e82c6d8265b
bomaiduo <bomaiduo@gmail.com>
2023-11-08 00:40:24 +0800

    Load Game 加载游戏进度

--- Assets/Scirpts/Manager/GameManager.cs
+++ Assets/Scirpts/Manager/GameManager.cs
@@ -60,9 +60,27 @@ public class GameManager : MonoBehaviour
     public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        PlayerPrefs.DeleteKey("playerHealth");
+        //PlayerPrefs.DeleteKey("playerHealth");
     }
 
+    public void NewGame()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(1);
+    }
+
+    public void ContinueGame()
+    {
+        if(PlayerPrefs.HasKey("sceneIndex"))
+           SceneManager.LoadScene(PlayerPrefs.GetInt("sceneIndex"));
+        else
+           NewGame();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
@@ -89,6 +107,7 @@ public class GameManager : MonoBehaviour
     public void SaveData()
     {
         PlayerPrefs.SetFloat("playerHealth",player.health);
+        PlayerPrefs.SetInt("sceneIndex", SceneManager.GetActiveScene().buildIndex + 1);
         PlayerPrefs.Save();
     }
 }
